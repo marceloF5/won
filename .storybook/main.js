40d9df53f0585be11b216ae96f331cc4be64d1af
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-09-19 17:18:46 +0100

    feat: Migrate storybook to last version

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,4 @@
 module.exports = {
     stories: ['../src/components/**/stories.tsx'],
-    addons: [
-        '@storybook/preset-typescript',
-        '@storybook/addon-actions',
-        '@storybook/addon-links',
-        '@storybook/addon-knobs'
-    ],
-
+    addons: ['@storybook/addon-essentials']
 };
--- src/components/main/stories.tsx
+++ src/components/main/stories.tsx
@@ -1,19 +1,10 @@
-import { withKnobs, text } from '@storybook/addon-knobs'
+import { Story, Meta } from '@storybook/react/types-6-0'
 
 import Main from '.'
 
 export default {
     title: 'Main',
-    component: Main,
-    decorators: [withKnobs]
-}
+    component: Main
+} as Meta
 
-export const Basic = () => (
-    <Main
-        title={text('Title', 'Boilerplate NextJS')}
-        description={text(
-            'Description',
-            'with TypeScript, ReactJS, NextJS and Styled Components'
-        )}
-    />
-)
+export const Basic: Story = (args) => <Main {...args} />
