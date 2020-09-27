6abda0f6b5cc370e5c455c042273ef51ee566ace
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-09-27 01:24:18 +0100

    feat: Add menu component

---
+++ src/components/Menu/stories.tsx
@@ -0,0 +1,19 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import Menu from '.'
+
+export default {
+    title: 'Menu',
+    component: Menu
+} as Meta
+
+export const Default: Story = () => <Menu />
+
+Default.parameters = {
+    layout: 'fullscreen',
+    backgrounds: {
+        default: 'dark'
+    },
+    viewport: {
+        defaultViewport: 'mobile1'
+    }
+}
