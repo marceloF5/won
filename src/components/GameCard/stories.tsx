e3ee150146c1b034d33557e3b6766b87b703635c
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-10-05 14:22:25 +0100

    feat: Add GameCard stories

--- src/components/GameCard/stories.tsx
+++ src/components/GameCard/stories.tsx
@@ -1,9 +1,37 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import GameCard from '.'
+import GameCard, { GameCardProps } from '.'
 
 export default {
     title: 'GameCard',
-    component: GameCard
+    component: GameCard,
+    args: {
+        title: 'Population Zero',
+        developer: 'Rockestar Games',
+        img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+        price: '€ 65.00'
+    },
+    parameters: {
+        backgrounds: {
+            default: 'dark'
+        }
+    }
 } as Meta
 
-export const Default: Story = () => <GameCard />
+export const Default: Story<GameCardProps> = (args) => (
+    <div style={{ width: '30rem' }}>
+        <GameCard {...args} />
+    </div>
+)
+
+export const WithRibbon: Story<GameCardProps> = (args) => (
+    <div style={{ width: '30rem' }}>
+        <GameCard {...args} />
+    </div>
+)
+
+WithRibbon.args = {
+    promotionalPrice: '€ 20.00',
+    ribbon: '80% OFF',
+    ribbonSize: 'small',
+    ribbonColor: 'primary'
+}
