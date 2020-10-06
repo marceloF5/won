d7f0419af386508208e0b7fc761d4370c51ccbc8
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-10-07 00:11:23 +0100

    feat: Implement Slider stories

--- src/components/Slider/stories.tsx
+++ src/components/Slider/stories.tsx
@@ -1,4 +1,7 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
+import { Settings } from 'react-slick'
+import styled from 'styled-components'
+
 import Slider from '.'
 
 export default {
@@ -6,4 +9,47 @@ export default {
     component: Slider
 } as Meta
 
-export const Default: Story = () => <Slider />
+const horizontalSettings: Settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1
+}
+
+const verticalSettings: Settings = {
+    vertical: true,
+    verticalSwiping: true,
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+}
+
+const Slide = styled.div`
+    background: gray;
+    border: 0.1rem solid red;
+    color: white;
+    padding: 10rem 0;
+    text-align: center;
+    width: 30rem;
+`
+
+export const Horizontal: Story = () => (
+    <Slider settings={horizontalSettings}>
+        <Slide>1</Slide>
+        <Slide>2</Slide>
+        <Slide>3</Slide>
+        <Slide>4</Slide>
+    </Slider>
+)
+
+export const Vertical: Story = () => (
+    <Slider settings={verticalSettings}>
+        <Slide>1</Slide>
+        <Slide>2</Slide>
+        <Slide>3</Slide>
+        <Slide>4</Slide>
+    </Slider>
+)
