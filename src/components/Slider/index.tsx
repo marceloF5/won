9caf95e51c7f3955ca2b6553f2c35e888bfbbdd0
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-10-07 00:10:52 +0100

    feat: Implement Slider component

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,9 +1,20 @@
+import SlickSlider, { Settings } from 'react-slick'
+
 import * as S from './styles'
 
-const Slider = () => (
-    <S.Wrapper>
-        <h1>Slider</h1>
-    </S.Wrapper>
-)
+export type SliderSettings = Settings
+
+export type SliderProps = {
+    children: React.ReactNode
+    settings: SliderSettings
+}
+
+const Slider = ({ children, settings }: SliderProps) => {
+    return (
+        <S.Wrapper>
+            <SlickSlider {...settings}>{children}</SlickSlider>
+        </S.Wrapper>
+    )
+}
 
 export default Slider
