2570dec5a236f77dd747b6c5e67d276d2c91ef7a
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-10-10 19:58:34 +0100

    feat: Implement BannerSlider

--- src/components/BannerSlider/index.tsx
+++ src/components/BannerSlider/index.tsx
@@ -1,8 +1,21 @@
+import Banner, { BannerProps } from 'components/Banner'
+import Slider from 'components/Slider'
+import React from 'react'
 import * as S from './styles'
 
-const BannerSlider = () => (
+export type BannerSliderProps = {
+    items: BannerProps[]
+}
+
+const settings = {}
+
+const BannerSlider = ({ items }: BannerSliderProps) => (
     <S.Wrapper>
-        <h1>BannerSlider</h1>
+        <Slider settings={settings}>
+            {items.map((item) => (
+                <Banner key={item.title} {...item} />
+            ))}
+        </Slider>
     </S.Wrapper>
 )
 
