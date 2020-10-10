9f3c7400ff4a727837458277837f87da801c6385
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-10-11 00:37:31 +0100

    chore: Change background setting in storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,10 +2,26 @@ import { ThemeProvider } from 'styled-components'
 import GlobalStyles from '../src/styles/global'
 import theme from '../src/styles/theme'
 
+export const parameters = {
+    backgrounds: {
+        default: 'Won Games',
+        values: [
+            {
+                name: 'won-light',
+                value: theme.colors.white
+            },
+            {
+                name: 'won-dark',
+                value: theme.colors.mainbg
+            },
+          ],
+    }
+}
+
 export const decorators = [
     (Story) => (
         <ThemeProvider theme={theme}>
-            <GlobalStyles />
+            <GlobalStyles removeBg />
             <Story />
         </ThemeProvider>
     )
