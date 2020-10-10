d14c2cb6844070ef4e8e9bb400436776d5d8e446
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-10-11 00:39:26 +0100

    chore: Create mock to components

---
+++ src/components/BannerSlider/mock.ts
@@ -0,0 +1,17 @@
+export default [
+    {
+        img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+        title: 'Defy death 1',
+        subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+        buttonLabel: 'Buy now',
+        buttonLink: '/games/defy-death',
+        ribbon: 'Bestselling'
+    },
+    {
+        img: 'https://source.unsplash.com/user/willianjusten/1042x582',
+        title: 'Defy death 2',
+        subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+        buttonLabel: 'Buy now',
+        buttonLink: '/games/defy-death'
+    }
+]
---
+++ src/components/Highlight/mock.ts
@@ -0,0 +1,7 @@
+export default {
+    title: 'Read Dead is Back!',
+    subtitle: 'Come see John new adventures',
+    backgroundImage: 'https://source.unsplash.com/user/willianjusten/1042x580',
+    buttonLabel: 'Buy Now',
+    buttonLink: '/games'
+}
