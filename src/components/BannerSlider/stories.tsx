88d8a345fcf492a0b335962eb85370966239b033
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-10-13 19:50:35 +0100

    feat: Add BannerSlider stories

--- src/components/BannerSlider/stories.tsx
+++ src/components/BannerSlider/stories.tsx
@@ -1,9 +1,22 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import BannerSlider from '.'
+import BannerSlider, { BannerSliderProps } from '.'
+
+import items from './mock'
 
 export default {
     title: 'BannerSlider',
-    component: BannerSlider
+    component: BannerSlider,
+    args: { items },
+    parameters: {
+        layout: 'fullscreen',
+        backgrounds: {
+            default: 'won-dark'
+        }
+    }
 } as Meta
 
-export const Default: Story = () => <BannerSlider />
+export const Default: Story<BannerSliderProps> = (args) => (
+    <div style={{ maxWidth: '130rem', margin: '0 auto' }}>
+        <BannerSlider {...args} />
+    </div>
+)
