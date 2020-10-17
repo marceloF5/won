68ad97d495c407e9ed6a54a202773edce5ac636b
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-10-17 15:12:53 +0100

    feat: Add GameCardSlider stories

---
+++ src/components/GameCardSlider/stories.tsx
@@ -0,0 +1,23 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import { GameCardProps } from 'components/GameCard'
+import GameCardSlider from '.'
+
+import items from './mock'
+
+export default {
+    title: 'GameCardSlider',
+    component: GameCardSlider,
+    args: { items },
+    parameters: {
+        layout: 'fullscreen',
+        backgrounds: {
+            default: 'won-dark'
+        }
+    }
+} as Meta
+
+export const Default: Story<GameCardProps[]> = (args) => (
+    <div style={{ maxWidth: '130rem', margin: '0 auto' }}>
+        <GameCardSlider items={args} {...args} />
+    </div>
+)
