48fa70af7f2c7ad511333831ce1450a6d9b87bc4
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-10-19 11:13:20 +0100

    chore: General changes on the _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,13 +10,13 @@ function App({ Component, pageProps }: AppProps) {
     return (
         <ThemeProvider theme={theme}>
             <Head>
-                <title>Boilerplate</title>
+                <title>Won Games</title>
                 <link rel="shortcut icon" href="/img/icon-512.png" />
                 <link rel="apple-touch-icon" href="/img/icon-512.png" />
                 <link rel="manifest" href="/manifest.json" />
                 <meta
                     name="description"
-                    content="A simple project starter to work with Typescript, React, Nextjs and Styled Components "
+                    content="The best Game Stores in the world"
                 />
             </Head>
             <GlobalStyles />
