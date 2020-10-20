19260776637e8b4433a492f82913680d894f0fe5
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-10-20 21:28:24 +0100

    feat: Add test to new prop

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -60,6 +60,23 @@ describe('<Button />', () => {
         expect(screen.getByTestId(/icon/i)).toBeInTheDocument()
     })
 
+    it('should render a minimal button version', () => {
+        renderWithTheme(
+            <Button icon={<AddShoppingCart data-testid="icon" />} minimal>
+                Buy now
+            </Button>
+        )
+
+        expect(screen.getByRole('button', { name: /buy now/i })).toHaveStyle({
+            background: 'none',
+            color: '#F231A5'
+        })
+
+        expect(
+            screen.getByRole('button', { name: /buy now/i })
+        ).toHaveStyleRule('background', 'none', { modifier: ':hover' })
+    })
+
     it('should render button as link', () => {
         renderWithTheme(
             <Button as="a" href="/link">
