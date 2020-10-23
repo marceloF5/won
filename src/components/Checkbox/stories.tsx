05f7ba7f0b6da002c7e1e92674da60341600f3c3
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-10-23 12:06:31 +0100

    feat: Add Checkbox stories

---
+++ src/components/Checkbox/stories.tsx
@@ -0,0 +1,12 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import Checkbox, { CheckboxProps } from '.'
+
+export default {
+    title: 'Checkbox',
+    component: Checkbox,
+    argTypes: {
+        onCheck: { action: 'checked' }
+    }
+} as Meta
+
+export const Default: Story<CheckboxProps> = (args) => <Checkbox {...args} />
