5d432046d37fe8b9a99d34dd40fdbaf94cc08f5d
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-10-31 10:28:58 +0000

    feat: Add parameters and improvements in layout

--- src/components/Checkbox/stories.tsx
+++ src/components/Checkbox/stories.tsx
@@ -4,9 +4,40 @@ import Checkbox, { CheckboxProps } from '.'
 export default {
     title: 'Checkbox',
     component: Checkbox,
+    parameters: {
+        layout: 'fullscreen',
+        backgrounds: {
+            default: 'won-dark'
+        }
+    },
     argTypes: {
         onCheck: { action: 'checked' }
     }
 } as Meta
 
-export const Default: Story<CheckboxProps> = (args) => <Checkbox {...args} />
+export const Default: Story<CheckboxProps> = (args) => (
+    <>
+        <Checkbox
+            style={{ marginBottom: '1rem' }}
+            name="category"
+            label="Action"
+            labelFor="action"
+            isChecked
+            {...args}
+        />
+        <Checkbox
+            style={{ marginBottom: '1rem' }}
+            name="category"
+            label="Adventure"
+            labelFor="adventure"
+            {...args}
+        />
+        <Checkbox
+            style={{ marginBottom: '1rem' }}
+            name="category"
+            label="Strategy"
+            labelFor="strategy"
+            {...args}
+        />
+    </>
+)
