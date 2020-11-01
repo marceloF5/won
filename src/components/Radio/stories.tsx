c377ac46b9d991a1190404a492f780c2d8dab7e9
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-11-01 09:25:38 +0000

    feat: Implement stories radio button

--- src/components/Radio/stories.tsx
+++ src/components/Radio/stories.tsx
@@ -1,9 +1,49 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import Radio from '.'
+import Radio, { RadioProps } from '.'
 
 export default {
     title: 'Radio',
+    parameters: {
+        layout: 'fullscreen',
+        backgrounds: {
+            default: 'won-dark'
+        }
+    },
+    argsTypes: {
+        onCheck: { action: 'checked' }
+    },
     component: Radio
 } as Meta
 
-export const Default: Story = () => <Radio />
+export const Default: Story<RadioProps> = (args) => (
+    <>
+        <Radio
+            style={{ marginTop: '1rem' }}
+            label="first"
+            labelFor="first"
+            id="first"
+            name="Name"
+            value="First"
+            defaultChecked
+            {...args}
+        />
+        <Radio
+            style={{ marginTop: '1rem' }}
+            label="second"
+            labelFor="second"
+            id="second"
+            name="Name"
+            value="Second"
+            {...args}
+        />
+        <Radio
+            style={{ marginTop: '1rem' }}
+            label="third"
+            labelFor="third"
+            id="third"
+            name="Name"
+            value="Third"
+            {...args}
+        />
+    </>
+)
