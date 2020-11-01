443934a655afa46d0acfaf35b74a398ef0cdf0af
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-11-01 09:26:46 +0000

    feat: Implement tests radio button

--- src/components/Radio/test.tsx
+++ src/components/Radio/test.tsx
@@ -1,15 +1,62 @@
-import { render, screen } from '@testing-library/react'
+import { screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import React from 'react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Radio from '.'
 
 describe('<Radio />', () => {
-    it('should render the heading', () => {
-        const { container } = render(<Radio />)
+    it('should render without label', () => {
+        renderWithTheme(<Radio />)
 
-        expect(
-            screen.getByRole('heading', { name: /Radio/i })
-        ).toBeInTheDocument()
+        expect(screen.queryByLabelText('Radio')).not.toBeInTheDocument()
+    })
 
-        expect(container.firstChild).toMatchSnapshot()
+    it('should render with white label', () => {
+        renderWithTheme(
+            <Radio label="radio label" labelFor="check" value="any value" />
+        )
+
+        expect(screen.getByRole('radio')).toBeInTheDocument()
+        expect(screen.getByLabelText(/radio label/i)).toBeInTheDocument()
+        expect(screen.getByText(/radio label/i)).toHaveStyle({
+            color: '#fafafa'
+        })
+    })
+
+    it('should render with black label', () => {
+        renderWithTheme(
+            <Radio label="radio label" labelFor="check" labelColor="black" />
+        )
+
+        expect(screen.queryByText(/radio label/i)).toHaveStyle({
+            color: '#030517'
+        })
+    })
+
+    it('should dispath onCheck when status changes', async () => {
+        const onCheck = jest.fn()
+        renderWithTheme(
+            <Radio label="Radio" onCheck={onCheck} value="any value" />
+        )
+
+        expect(onCheck).not.toHaveBeenCalled()
+
+        userEvent.click(screen.getByRole('radio'))
+        await waitFor(() => {
+            expect(onCheck).toHaveBeenCalledTimes(1)
+        })
+
+        expect(onCheck).toHaveBeenCalledWith('any value')
+    })
+
+    it('should be accessible with tab', async () => {
+        renderWithTheme(<Radio label="Radio" labelFor="radio" />)
+
+        expect(document.body).toHaveFocus()
+
+        userEvent.tab()
+
+        expect(screen.getByLabelText(/radio/i)).toHaveFocus()
     })
 })
