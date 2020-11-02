9d13bf89dfd8cacd75c8f5d1375d8db4fde23294
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-11-02 09:22:23 +0000

    chore: Add templates in storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-    stories: ['../src/components/**/stories.tsx'],
+    stories: ['../src/components/**/stories.tsx', '../src/templates/**/stories.tsx'],
     addons: ['@storybook/addon-essentials'],
     webpackFinal: (config) => {
         config.resolve.modules.push(`${process.cwd()}/src`)
