4c6b9402b28ad088eb2d8728359b0a74a5f2a26c
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-11-10 09:54:46 +0000

    feat: Implement TextField stories

---
+++ src/components/TextField/stories.tsx
@@ -0,0 +1,45 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import { Email } from '@styled-icons/material-outlined'
+import TextField, { TextFieldProps } from '.'
+
+export default {
+    title: 'atoms/Form/TextField',
+    component: TextField,
+    args: {
+        disabled: false,
+        label: 'E-mail',
+        labelFor: 'email',
+        id: 'email',
+        initialValue: 'mp.fortunato88@gmail.com',
+        placeholder: 'E-mail'
+    },
+    argTypes: {
+        onInput: { action: 'changed' },
+        icon: { type: '' }
+    }
+} as Meta
+
+export const Default: Story<TextFieldProps> = (args) => (
+    <div style={{ maxWidth: 300, padding: 15 }}>
+        <TextField {...args} />
+    </div>
+)
+
+export const withIcon: Story<TextFieldProps> = (args) => (
+    <div style={{ maxWidth: 300, padding: 15 }}>
+        <TextField {...args} />
+    </div>
+)
+withIcon.args = {
+    icon: <Email />
+}
+
+export const withError: Story<TextFieldProps> = (args) => (
+    <div style={{ maxWidth: 300, padding: 15 }}>
+        <TextField {...args} />
+    </div>
+)
+withError.args = {
+    icon: <Email />,
+    error: 'Opps... invalid field'
+}
