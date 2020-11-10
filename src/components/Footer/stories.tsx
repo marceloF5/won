1e30d290763e97a515169ee825481bb437799c4a
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-11-10 08:34:11 +0000

    chore: Changes Footer stories

--- src/components/Footer/stories.tsx
+++ src/components/Footer/stories.tsx
@@ -2,7 +2,7 @@ import { Story, Meta } from '@storybook/react/types-6-0'
 import Footer from '.'
 
 export default {
-    title: 'Footer',
+    title: 'Module/Footer',
     component: Footer
 } as Meta
 
