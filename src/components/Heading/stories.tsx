7bc1ca2febadbd606ef2004ee9ab3639cfc42f2e
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-11-13 08:59:33 +0000

    chore: Change Heading stories

--- src/components/Heading/stories.tsx
+++ src/components/Heading/stories.tsx
@@ -2,7 +2,7 @@ import { Story, Meta } from '@storybook/react/types-6-0'
 import Heading, { HeadingProps } from '.'
 
 export default {
-    title: 'Heading',
+    title: 'atoms/Heading',
     component: Heading,
     argTypes: {
         children: {
