c1b3191f7b89b0e297ad649ef92800ddaba48a37
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-11-13 08:45:47 +0000

    feat: Change GameCard stories

--- src/components/GameCard/stories.tsx
+++ src/components/GameCard/stories.tsx
@@ -2,7 +2,7 @@ import { Story, Meta } from '@storybook/react/types-6-0'
 import GameCard, { GameCardProps } from '.'
 
 export default {
-    title: 'GameCard',
+    title: 'Module/GameCard',
     component: GameCard,
     args: {
         title: 'Population Zero',
