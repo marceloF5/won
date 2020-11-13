ec68889bfff0900254a683e5f1505f6eee433645
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-11-13 08:46:25 +0000

    feat: Change GameCardSlider stories

--- src/components/GameCardSlider/stories.tsx
+++ src/components/GameCardSlider/stories.tsx
@@ -5,7 +5,7 @@ import GameCardSlider from '.'
 import items from './mock'
 
 export default {
-    title: 'GameCardSlider',
+    title: 'Module/GameCardSlider',
     component: GameCardSlider,
     args: { items },
     parameters: {
