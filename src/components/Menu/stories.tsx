8415f1ac629cae52b9c0fca400b891054c3fab89
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-11-15 10:17:02 +0000

    chore: Change Menu stories

--- src/components/Menu/stories.tsx
+++ src/components/Menu/stories.tsx
@@ -2,7 +2,7 @@ import { Story, Meta } from '@storybook/react/types-6-0'
 import Menu, { MenuProps } from '.'
 
 export default {
-    title: 'Menu',
+    title: 'Module/Menu',
     component: Menu
 } as Meta
 
