76dbd80173c1b0000defa7de9e7a76aef895a46d
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-11-15 10:11:56 +0000

    chore: Change Logo stories

--- src/components/Logo/stories.tsx
+++ src/components/Logo/stories.tsx
@@ -2,7 +2,7 @@ import { Story, Meta } from '@storybook/react/types-6-0'
 import Logo, { LogoProps } from '.'
 
 export default {
-    title: 'Logo',
+    title: 'atoms/Logo',
     component: Logo
 } as Meta
 
