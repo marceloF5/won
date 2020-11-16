3ed2f348d26e1e976125d31bcf85e6b1655900ed
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-11-16 09:12:06 +0000

    chore: Change Menu unit tests

--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -39,7 +39,7 @@ describe('<Menu />', () => {
 
         expect(screen.queryByText(/my account/i)).not.toBeInTheDocument()
         expect(screen.queryByText(/wishlist/i)).not.toBeInTheDocument()
-        expect(screen.getByText(/log in now/i)).toBeInTheDocument()
+        expect(screen.getByText(/sign in now/i)).toBeInTheDocument()
         expect(screen.getByText(/sign up/i)).toBeInTheDocument()
     })
 
