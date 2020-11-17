0ce28037adad0603369518e420b7d65dc22b497b
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-11-17 10:47:10 +0000

    feat: Add Auth template

---
+++ src/templates/Auth/index.tsx
@@ -0,0 +1,51 @@
+import Link from 'next/link'
+
+import * as S from './styles'
+import Logo from 'components/Logo'
+import Heading from 'components/Heading'
+
+export type AuthProps = {
+    title: string
+    children: React.ReactNode
+}
+
+const Auth = ({ title, children }: AuthProps) => (
+    <S.Wrapper>
+        <S.BannerBlock>
+            <S.BannerContent>
+                <Link href="/">
+                    <a>
+                        <Logo id="banner" />
+                    </a>
+                </Link>
+                <div>
+                    <Heading size="huge">
+                        All your favorite games in one place
+                    </Heading>
+                    <S.Subtitle>
+                        <strong>WON</strong> is the best and most complete
+                        gaming platform.
+                    </S.Subtitle>
+                </div>
+                <S.Footer>
+                    Won Games 2020 © Todos os Direitos Reservados
+                </S.Footer>
+            </S.BannerContent>
+        </S.BannerBlock>
+        <S.Content>
+            <S.ContentWrapper>
+                <Link href="/">
+                    <a>
+                        <Logo color="black" id="content" size="large" />
+                    </a>
+                </Link>
+                <Heading color="black" lineColor="secondary" lineLeft>
+                    {title}
+                </Heading>
+                {children}
+            </S.ContentWrapper>
+        </S.Content>
+    </S.Wrapper>
+)
+
+export default Auth
