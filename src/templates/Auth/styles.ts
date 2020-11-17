c8d97c916496c69c047bfbc7429da62c7a15f6c3
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-11-17 11:00:50 +0000

    feat: Add Auth styles

---
+++ src/templates/Auth/styles.ts
@@ -0,0 +1,108 @@
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
+
+import * as HeadingStyles from 'components/Heading/styles'
+import * as LogoStyles from 'components/Logo/styles'
+
+export const Wrapper = styled.main`
+    display: grid;
+    grid-template-columns: 1fr;
+    height: 100vh;
+
+    ${media.greaterThan('medium')`
+        grid-template-columns: 1fr 1fr;
+    `}
+`
+
+export const BannerBlock = styled.div`
+    ${({ theme }) => css`
+        background-image: url(/img/auth-bg.jpg);
+        background-size: cover;
+        background-position: center center;
+        position: relative;
+        padding: ${theme.spacings.xxlarge} ${theme.spacings.xxlarge}
+            ${theme.spacings.large};
+
+        ${media.lessThan('medium')`
+            display: none;
+
+        `}
+
+        &:after {
+            content: '';
+            position: absolute;
+            top: 0;
+            bottom: 0;
+            left: 0;
+            right: 0;
+            background: ${theme.colors.black};
+            opacity: 0.85;
+        }
+    `}
+`
+
+export const BannerContent = styled.div`
+    ${({ theme }) => css`
+        color: ${theme.colors.white};
+        z-index: ${theme.layers.base};
+
+        height: 100%;
+        position: relative;
+        display: grid;
+        grid-template-columns: 1fr;
+        justify-content: space-between;
+
+        a {
+            width: fit-content;
+            height: fit-content;
+        }
+    `}
+`
+
+export const Subtitle = styled.h3`
+    ${({ theme }) => css`
+        font-size: ${theme.font.sizes.xxlarge};
+        font-weight: ${theme.font.light};
+        margin-top: ${theme.spacings.xxsmall};
+
+        strong {
+            color: ${theme.colors.primary};
+        }
+    `}
+`
+
+export const Footer = styled.p`
+    ${({ theme }) => css`
+        font-size: ${theme.font.sizes.xsmall};
+        align-self: end;
+        text-align: center;
+    `}
+`
+
+export const Content = styled.div`
+    ${({ theme }) => css`
+        background-color: ${theme.colors.white};
+
+        display: grid;
+        align-items: center;
+        justify-content: center;
+    `}
+`
+
+export const ContentWrapper = styled.div`
+    ${({ theme }) => css`
+        width: 30rem;
+
+        ${media.greaterThan('medium')`
+            width: 36rem;
+        `}
+
+        ${LogoStyles.Wrapper} {
+            margin: 0 auto ${theme.spacings.xxlarge};
+        }
+
+        ${HeadingStyles.Wrapper} {
+            margin-bottom: ${theme.spacings.medium};
+        }
+    `}
+`
