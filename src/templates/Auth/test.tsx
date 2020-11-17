e05f2d322b149ec6fcb9b27a41b1e77231f71351
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-11-17 11:01:25 +0000

    feat: Add Auth unit tests

---
+++ src/templates/Auth/test.tsx
@@ -0,0 +1,36 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Auth from '.'
+
+describe('<Auth />', () => {
+    it('should render the heading', () => {
+        renderWithTheme(
+            <Auth title="Auth Title">
+                <input type="text" />
+            </Auth>
+        )
+
+        expect(screen.getAllByRole('img', { name: /won games/i })).toHaveLength(
+            2
+        )
+
+        expect(
+            screen.getByRole('heading', {
+                name: /All your favorite games in one place/i
+            })
+        ).toBeInTheDocument()
+
+        expect(
+            screen.getByRole('heading', {
+                name: /won is the best and most complete gaming platform/i
+            })
+        ).toBeInTheDocument()
+
+        expect(
+            screen.getByRole('heading', { name: /auth title/i })
+        ).toBeInTheDocument()
+
+        expect(screen.getByRole('textbox')).toBeInTheDocument()
+    })
+})
