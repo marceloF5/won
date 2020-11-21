e6ad023d4f3cd72d1001279a1dd4b992f50bc605
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-11-21 21:12:09 +0000

    feat: Implement Showcase component

--- src/components/Showcase/index.tsx
+++ src/components/Showcase/index.tsx
@@ -1,8 +1,26 @@
-import * as S from './styles'
+import GameCardSlider from 'components/GameCardSlider'
+import { GameCardProps } from 'components/GameCard'
+import Heading from 'components/Heading'
+import Highlight, { HighlightProps } from 'components/Highlight'
 
-const Showcase = () => (
+import * as S from './styles'
+import React from 'react'
+
+export type ShowCaseProps = {
+    title?: string
+    highlight?: HighlightProps
+    games?: GameCardProps[]
+}
+
+const Showcase = ({ title, highlight, games }: ShowCaseProps) => (
     <S.Wrapper>
-        <h1>Showcase</h1>
+        {!!title && (
+            <Heading lineLeft lineColor="secondary">
+                {title}
+            </Heading>
+        )}
+        {!!highlight && <Highlight {...highlight} />}
+        {!!games && <GameCardSlider items={games} />}
     </S.Wrapper>
 )
 
