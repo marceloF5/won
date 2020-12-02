8ae07d32fef5b33d8e7f392c248f7a482ed74aaf
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-12-02 21:39:21 +0000

    feat: Implement GameInfo unit test

--- src/components/GameInfo/test.tsx
+++ src/components/GameInfo/test.tsx
@@ -1,15 +1,34 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import GameInfo from '.'
 
+const props = {
+    title: 'My game title',
+    description: 'My game description',
+    price: '210.00'
+}
+
 describe('<GameInfo />', () => {
-    it('should render the heading', () => {
-        const { container } = render(<GameInfo />)
+    it('should render the game information', () => {
+        const { container } = renderWithTheme(<GameInfo {...props} />)
 
         expect(
-            screen.getByRole('heading', { name: /GameInfo/i })
+            screen.getByRole('heading', { name: /my game title/i })
         ).toBeInTheDocument()
-
+        expect(screen.getByText(/my game description/i)).toBeInTheDocument()
+        expect(screen.getByText(/\$210.00/)).toBeInTheDocument()
         expect(container.firstChild).toMatchSnapshot()
+    })
+
+    it('should render buttons', () => {
+        renderWithTheme(<GameInfo {...props} />)
+
+        expect(
+            screen.getByRole('button', { name: /add to cart/i })
+        ).toBeInTheDocument()
+        expect(
+            screen.getByRole('button', { name: /wishlist/i })
+        ).toBeInTheDocument()
     })
 })
