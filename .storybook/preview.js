925b10aa38726e0a72febc2d06da9f975d17a270
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-12-05 21:25:47 +0000

    chore: Update setting of the storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,10 @@
+import { addDecorator } from '@storybook/react'
+import { withNextRouter } from 'storybook-addon-next-router'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyles from '../src/styles/global'
 import theme from '../src/styles/theme'
+
+addDecorator(withNextRouter())
 
 export const parameters = {
     backgrounds: {
