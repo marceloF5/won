90f433fcbc96bf35ff40f4ad2df010cccf468b67
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-12-05 20:48:50 +0000

    feat: Implement GameDetails

--- src/components/GameDetails/index.tsx
+++ src/components/GameDetails/index.tsx
@@ -1,12 +1,67 @@
+import { Apple, Windows, Linux } from '@styled-icons/fa-brands'
+
 import Heading from 'components/Heading'
+import MediaMatch from 'components/MediaMatch'
 import * as S from './styles'
 
-const GameDetails = () => (
-    <S.Wrapper>
-        <Heading lineLeft lineColor="secondary">
-            Game Details
-        </Heading>
-    </S.Wrapper>
-)
+type Platform = 'linux' | 'mac' | 'windows'
+
+export type GameDetailPlatformProps = {
+    platforms: Platform[]
+}
+
+const GameDetails = ({ platforms }: GameDetailPlatformProps) => {
+    const platformIcons = {
+        linux: <Linux title="Linux" size={18} />,
+        mac: <Apple title="Mac" size={18} />,
+        windows: <Windows title="Windows" size={18} />
+    }
+
+    return (
+        <S.Wrapper>
+            <MediaMatch greaterThan="small">
+                <Heading lineLeft lineColor="secondary">
+                    Game Details
+                </Heading>
+            </MediaMatch>
+
+            <S.Content>
+                <S.Block>
+                    <S.Label>Developer</S.Label>
+                    <S.Description>Gearbox Software</S.Description>
+                </S.Block>
+
+                <S.Block>
+                    <S.Label>Release Date</S.Label>
+                    <S.Description>Nov 16, 2019</S.Description>
+                </S.Block>
+
+                <S.Block>
+                    <S.Label>Platforms</S.Label>
+                    <S.IconsWrapper>
+                        {platforms.map((icon: Platform) => (
+                            <S.Icon key={icon}>{platformIcons[icon]}</S.Icon>
+                        ))}
+                    </S.IconsWrapper>
+                </S.Block>
+
+                <S.Block>
+                    <S.Label>Publisher</S.Label>
+                    <S.Description>2k</S.Description>
+                </S.Block>
+
+                <S.Block>
+                    <S.Label>Rating</S.Label>
+                    <S.Description>18+</S.Description>
+                </S.Block>
+
+                <S.Block>
+                    <S.Label>Genres</S.Label>
+                    <S.Description>Actions / Adventure</S.Description>
+                </S.Block>
+            </S.Content>
+        </S.Wrapper>
+    )
+}
 
 export default GameDetails
