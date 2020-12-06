66ac81a91b5ffef54c2e85d8bceea6cfa9a32fec
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-12-06 17:13:48 +0000

    feat: Add mock to GameDetails

---
+++ src/components/GameDetails/mock.ts
@@ -0,0 +1,8 @@
+export default {
+    developer: 'Different Tales',
+    genres: ['Role-playing'],
+    platforms: ['linux', 'mac', 'windows'],
+    publisher: 'Walkabout',
+    rating: 'BR0',
+    releaseDate: '2020-11-21T23:00:00'
+}
