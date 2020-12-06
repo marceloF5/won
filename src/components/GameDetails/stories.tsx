cf57a72dac12d0fd0d046feb2fa03c5344c933d6
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-12-06 17:14:22 +0000

    feat: Add GameDetails stories

--- src/components/GameDetails/stories.tsx
+++ src/components/GameDetails/stories.tsx
@@ -1,5 +1,6 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import GameDetails from '.'
+import GameDetails, { GameDetailsProps } from '.'
+import mockGames from './mock'
 
 export default {
     title: 'GameDetails',
@@ -8,7 +9,31 @@ export default {
         backgrounds: {
             default: 'won-dark'
         }
+    },
+    args: mockGames,
+    argTypes: {
+        genres: {
+            control: {
+                type: 'inline-check',
+                options: ['Role-playing', 'Narrative']
+            }
+        },
+        platforms: {
+            control: {
+                type: 'inline-check',
+                options: ['windows', 'linux', 'mac']
+            }
+        },
+        releaseDate: {
+            control: {
+                type: 'date'
+            }
+        }
     }
 } as Meta
 
-export const Default: Story = () => <GameDetails />
+export const Default: Story<GameDetailsProps> = (args) => (
+    <div style={{ maxWidth: '130rem', margin: '0 auto' }}>
+        <GameDetails {...args} />
+    </div>
+)
