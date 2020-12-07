b8a0c46e87a2105ab0a00f5bc03c828b570e9cd9
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-12-07 17:02:16 +0000

    feat: Implement TextContent styles

--- src/components/Heading/styles.ts
+++ src/components/Heading/styles.ts
@@ -2,7 +2,7 @@ import styled, { css, DefaultTheme } from 'styled-components'
 import media from 'styled-media-query'
 import { HeadingProps, LineColors } from '.'
 
-const wrapperModifiers = {
+export const wrapperModifiers = {
     lineBottom: (theme: DefaultTheme, lineColor: LineColors) => css`
         position: relative;
         margin-bottom: ${theme.spacings.medium};
