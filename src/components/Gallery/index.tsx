0ce36e445f941fbfa919d11d4ee968b35791500c
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-12-10 20:32:04 +0000

    feat: Create Gallery component structure

---
+++ src/components/Gallery/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles'
+
+const Gallery = () => (
+    <S.Wrapper>
+        <h1>Gallery</h1>
+    </S.Wrapper>
+)
+
+export default Gallery
---
+++ src/components/Gallery/test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react'
+
+import Gallery from '.'
+
+describe('<Gallery />', () => {
+    it('should render the heading', () => {
+        const { container } = render(<Gallery />)
+
+        expect(
+            screen.getByRole('heading', { name: /Gallery/i })
+        ).toBeInTheDocument()
+
+        expect(container.firstChild).toMatchSnapshot()
+    })
+})
