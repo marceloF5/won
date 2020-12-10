e29022e29a47d0271c637dd2fdd7b663f8acc9fa
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-12-10 21:30:28 +0000

    chore: Refactor GameCardSlider

--- src/components/GameCardSlider/styles.ts
+++ src/components/GameCardSlider/styles.ts
@@ -7,12 +7,14 @@ type WrapperProps = Pick<GameCardSliderProps, 'color'>
 export const Wrapper = styled.section<WrapperProps>`
     ${({ theme, color }) => css`
         ${media.lessThan('huge')`
-      overflow-x: hidden;
-    `}
+            overflow-x: hidden;
+        `}
+
         .slick-track,
-    .slick-list {
+        .slick-list {
             display: flex;
         }
+
         .slick-slide > div {
             margin: 0 ${theme.spacings.xxsmall};
             flex: 1 0 auto;
@@ -21,16 +23,18 @@ export const Wrapper = styled.section<WrapperProps>`
         .slick-list {
             margin: 0 -${theme.spacings.xxsmall};
         }
+
         ${media.greaterThan('large')`
-      .slick-slide > div {
-        margin: 0 ${theme.spacings.xsmall};
-      }
-      .slick-list {
-        margin: 0 -${theme.spacings.xsmall};
-      }
-    `}
+            .slick-slide > div {
+                margin: 0 ${theme.spacings.xsmall};
+            }
+            .slick-list {
+                margin: 0 -${theme.spacings.xsmall};
+            }
+        `}
+
         .slick-prev,
-    .slick-next {
+        .slick-next {
             display: block;
             color: ${theme.colors[color!]};
             cursor: pointer;
@@ -41,12 +45,15 @@ export const Wrapper = styled.section<WrapperProps>`
             padding: 0;
             transform: translate(0, -50%);
         }
+
         .slick-prev {
             left: -${theme.spacings.xxlarge};
         }
+
         .slick-next {
             right: -${theme.spacings.xxlarge};
         }
+
         .slick-prev.slick-disabled,
         .slick-next.slick-disabled {
             visibility: hidden;
