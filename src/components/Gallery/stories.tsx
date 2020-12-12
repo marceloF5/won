3d15dfb2858e02db555cec70064e2748e578ecce
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-12-12 11:01:16 +0000

    feat: Implement Gallery stories

--- src/components/Gallery/stories.tsx
+++ src/components/Gallery/stories.tsx
@@ -1,9 +1,24 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import Gallery from '.'
+import Gallery, { GalleryProps } from '.'
+
+import items from './mock'
 
 export default {
     title: 'Gallery',
-    component: Gallery
+    component: Gallery,
+    args: {
+        items
+    },
+    parameters: {
+        layout: 'fullscreen',
+        backgrounds: {
+            default: 'won-dark'
+        }
+    }
 } as Meta
 
-export const Default: Story = () => <Gallery />
+export const Default: Story<GalleryProps> = (args) => (
+    <div style={{ maxWidth: '130rem', margin: '0 auto' }}>
+        <Gallery {...args} />
+    </div>
+)
