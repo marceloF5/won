155d223a9b351f4d5d1950e413142a91bd581ce1
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-12-12 11:09:54 +0000

    feat: Implement Gallery unit tests

--- src/components/Gallery/test.tsx
+++ src/components/Gallery/test.tsx
@@ -1,15 +1,20 @@
-import { render, screen } from '@testing-library/react'
+import 'match-media-mock'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Gallery from '.'
+import items from './mock'
 
 describe('<Gallery />', () => {
-    it('should render the heading', () => {
-        const { container } = render(<Gallery />)
+    it('should render thumbnails as buttons', () => {
+        renderWithTheme(<Gallery items={items.slice(0, 2)} />)
 
         expect(
-            screen.getByRole('heading', { name: /Gallery/i })
-        ).toBeInTheDocument()
+            screen.getByRole('button', { name: /thumb - gallery image 1/i })
+        ).toHaveAttribute('src', items[0].src)
 
-        expect(container.firstChild).toMatchSnapshot()
+        expect(
+            screen.getByRole('button', { name: /thumb - gallery image 2/i })
+        ).toHaveAttribute('src', items[1].src)
     })
 })
