4d85066b56c0f02baa8ff460088fb66ac7d54e0c
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-12-13 20:26:39 +0000

    feat: Implement Gallery unit test

--- src/components/Gallery/test.tsx
+++ src/components/Gallery/test.tsx
@@ -1,5 +1,5 @@
 import 'match-media-mock'
-import { screen } from '@testing-library/react'
+import { screen, fireEvent } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import Gallery from '.'
@@ -16,5 +16,71 @@ describe('<Gallery />', () => {
         expect(
             screen.getByRole('button', { name: /thumb - gallery image 2/i })
         ).toHaveAttribute('src', items[1].src)
+    })
+
+    it('should handle open modal', () => {
+        renderWithTheme(<Gallery items={items.slice(0, 2)} />)
+
+        const modal = screen.getByLabelText('modal')
+
+        expect(modal.getAttribute('aria-hidden')).toBe('true')
+        expect(modal).toHaveStyle({ opacity: 0, pointerEvents: 'none' })
+
+        fireEvent.click(
+            screen.getByRole('button', { name: /thumb - gallery image 1/i })
+        )
+
+        expect(modal.getAttribute('aria-hidden')).toBe('false')
+        expect(modal).toHaveStyle({ opacity: 1 })
+    })
+
+    it('should handle open modal with selected image', async () => {
+        renderWithTheme(<Gallery items={items.slice(0, 2)} />)
+
+        fireEvent.click(
+            screen.getByRole('button', { name: /thumb - gallery image 2/i })
+        )
+
+        const img = await screen.findByRole('img', { name: /gallery image 2/i })
+
+        expect(img.parentElement?.parentElement).toHaveClass('slick-active')
+    })
+
+    it('should handle close modal when overlay or button clicked', () => {
+        renderWithTheme(<Gallery items={items.slice(0, 2)} />)
+
+        const modal = screen.getByLabelText('modal')
+
+        fireEvent.click(
+            screen.getByRole('button', { name: /thumb - gallery image 1/i })
+        )
+
+        expect(modal.getAttribute('aria-hidden')).toBe('false')
+        expect(modal).toHaveStyle({ opacity: 1 })
+
+        fireEvent.click(screen.getByRole('button', { name: /close modal/i }))
+
+        expect(modal.getAttribute('aria-hidden')).toBe('true')
+        expect(modal).toHaveStyle({ opacity: 0, pointerEvents: 'none' })
+    })
+
+    it('should handle close modal when ESC button is pressed', () => {
+        const { container } = renderWithTheme(
+            <Gallery items={items.slice(0, 2)} />
+        )
+
+        const modal = screen.getByLabelText('modal')
+
+        fireEvent.click(
+            screen.getByRole('button', { name: /thumb - gallery image 1/i })
+        )
+
+        expect(modal.getAttribute('aria-hidden')).toBe('false')
+        expect(modal).toHaveStyle({ opacity: 1 })
+
+        fireEvent.keyUp(container, { key: 'Escape' })
+
+        expect(modal.getAttribute('aria-hidden')).toBe('true')
+        expect(modal).toHaveStyle({ opacity: 0, pointerEvents: 'none' })
     })
 })
