525da8c722164fb49bbec61440a3c5fa5ab1caf6
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-12-23 10:01:03 +0000

    feat: Implement Game styles

--- src/templates/Game/styles.ts
+++ src/templates/Game/styles.ts
@@ -1,3 +1,80 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
-export const Wrapper = styled.main``
+import { Container } from 'components/Container'
+
+type CoverProps = {
+    src: string
+}
+
+export const Main = styled.main`
+    margin-top: 20rem;
+
+    ${media.greaterThan('medium')`
+        margin-top: 45rem;
+    `}
+`
+
+export const Cover = styled.div<CoverProps>`
+    ${({ src }) => css`
+        background-image: url(${src});
+        background-position: top center;
+        background-size: cover;
+        height: 39.5rem;
+        opacity: 0.4;
+
+        position: absolute;
+        top: 0;
+        right: 0;
+        left: 0;
+
+        ${media.greaterThan('medium')`
+            height: 70rem;
+            clip-path: polygon(0 0, 100% 0, 100% 100%, 0 85%)
+        `}
+    `}
+`
+
+const Section = styled(Container).attrs({ as: 'section' })`
+    ${({ theme }) => css`
+        margin-bottom: ${theme.spacings.xlarge};
+
+        ${media.greaterThan('medium')`
+            margin-bottom: calc(${theme.spacings.xlarge} * 2);
+        `}
+    `}
+`
+
+export const SectionGameInfo = styled(Section)``
+
+export const SectionGallery = styled(Section)`
+    display: none;
+
+    ${media.greaterThan('medium')`
+        display: block;
+    `}
+`
+
+export const SectionDescription = styled(Section)`
+    ${({ theme }) => css`
+        .description__copyrights {
+            color: ${theme.colors.gray};
+            font-size: ${theme.font.sizes.xsmall};
+            margin-top: ${theme.spacings.medium};
+        }
+    `}
+`
+
+export const SectionGameDetails = styled(Section)`
+    ${({ theme }) => css`
+        > div {
+            padding-bottom: ${theme.spacings.xlarge};
+            border-bottom: 0.1rem solid rgba(181, 181, 181, 0.3);
+
+            ${media.greaterThan('medium')`
+                padding-bottom: calc(${theme.spacings.xxlarge} * 2)
+
+            `};
+        }
+    `}
+`
