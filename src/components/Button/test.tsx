b5f749e6a9114aacd9ed0e2d44a3db5286eeec47
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-12-27 16:15:56 +0000

    chore: Add new unit test to button

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -89,4 +89,12 @@ describe('<Button />', () => {
             '/link'
         )
     })
+
+    it('should render a disabled button', () => {
+        renderWithTheme(<Button disabled>Buy now</Button>)
+
+        expect(
+            screen.getByRole('button', { name: /buy now/i })
+        ).toHaveStyleRule('cursor', 'not-allowed', { modifier: ':disabled' })
+    })
 })
