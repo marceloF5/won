3b8ea8123f4ca238eebf3283ee648c5dbbbd25aa
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2021-01-05 11:00:05 +0000

    feat: Implement CardList unit tests

---
+++ src/components/CartList/test.tsx
@@ -0,0 +1,20 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import CardList from '.'
+import mockItems from './mock'
+
+const props = {
+    items: mockItems,
+    total: '€330.00'
+}
+
+describe('<CardList />', () => {
+    it('should render the card list', () => {
+        const { container } = renderWithTheme(<CardList {...props} />)
+
+        expect(screen.getAllByRole('heading')).toHaveLength(2)
+        expect(screen.getByText('€330.00')).toHaveStyle({ color: '#f231a5' })
+        expect(container.firstChild).toMatchSnapshot()
+    })
+})
