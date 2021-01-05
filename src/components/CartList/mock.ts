3e52beaa124884b6731155f70ae3340c3546f6ef
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2021-01-05 10:58:23 +0000

    feat: Implement CardList mock

---
+++ src/components/CartList/mock.ts
@@ -0,0 +1,12 @@
+export default [
+    {
+        img: 'https://source.unsplash.com/user/willianjusten',
+        title: 'Red Dead Redemption 2',
+        price: '€215.00'
+    },
+    {
+        img: 'https://source.unsplash.com/user/willianjusten',
+        title: 'Bordlands 3',
+        price: '€215.00'
+    }
+]
