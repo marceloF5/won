c32b4ba5256bb84fd1b4b8885a44d6654164407d
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2021-01-15 09:19:41 +0000

    feat: Implement ProfileMenu unit tests

---
+++ src/components/ProfileMenu/test.tsx
@@ -0,0 +1,34 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import ProfileMenu from '.'
+
+describe('<ProfileMenu />', () => {
+    it('should render the menu', () => {
+        const { container } = renderWithTheme(<ProfileMenu />)
+
+        expect(
+            screen.getByRole('link', { name: /my profile/i })
+        ).toBeInTheDocument()
+        expect(
+            screen.getByRole('link', { name: /my cards/i })
+        ).toBeInTheDocument()
+        expect(
+            screen.getByRole('link', { name: /my orders/i })
+        ).toBeInTheDocument()
+        expect(
+            screen.getByRole('link', { name: /sign out/i })
+        ).toBeInTheDocument()
+
+        expect(container.firstChild).toMatchSnapshot()
+    })
+
+    it('should render the menu with an active link defined', () => {
+        renderWithTheme(<ProfileMenu activeLink="/profile/cards" />)
+
+        expect(screen.getByRole('link', { name: /my cards/i })).toHaveStyle({
+            background: '#f231a5',
+            color: '#fafafa'
+        })
+    })
+})
