5b32bc2aead0adf9761731bf75e157fe4f59f032
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2021-01-15 09:17:54 +0000

    feat: Implement ProfileMenu component

---
+++ src/components/ProfileMenu/index.tsx
@@ -0,0 +1,50 @@
+import {
+    AccountCircle,
+    CreditCard,
+    FormatListBulleted,
+    ExitToApp
+} from '@styled-icons/material-outlined'
+import Link from 'next/link'
+import React from 'react'
+import * as S from './styles'
+
+export type ProfileMenuProps = {
+    activeLink?: '/profile/me' | '/profile/cards' | '/profile/orders' | string
+}
+
+const ProfileMenu = ({ activeLink }: ProfileMenuProps) => (
+    <S.Nav>
+        <Link href="/profile/me" passHref>
+            <S.Link isActive={activeLink === '/profile/me'} title="My profile">
+                <AccountCircle size={24} />
+                <span>My profile</span>
+            </S.Link>
+        </Link>
+
+        <Link href="/profile/cards" passHref>
+            <S.Link isActive={activeLink === '/profile/cards'} title="My cards">
+                <CreditCard size={24} />
+                <span>My cards</span>
+            </S.Link>
+        </Link>
+
+        <Link href="/profile/orders" passHref>
+            <S.Link
+                isActive={activeLink === '/profile/orders'}
+                title="My orders"
+            >
+                <FormatListBulleted size={24} />
+                <span>My orders</span>
+            </S.Link>
+        </Link>
+
+        <Link href="/logout" passHref>
+            <S.Link>
+                <ExitToApp size={24} />
+                <span>Sign out</span>
+            </S.Link>
+        </Link>
+    </S.Nav>
+)
+
+export default ProfileMenu
