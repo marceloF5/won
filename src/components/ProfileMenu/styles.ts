7dc0dc4e88a9d01381d7bf7766afbd1f8032a3da
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2021-01-15 09:18:48 +0000

    feat: Implement ProfileMenu styles

---
+++ src/components/ProfileMenu/styles.ts
@@ -0,0 +1,67 @@
+import styled, { css, DefaultTheme } from 'styled-components'
+import media from 'styled-media-query'
+
+type LinkProps = {
+    isActive?: boolean
+}
+
+const linkModifiers = {
+    default: (theme: DefaultTheme) => css`
+        background: ${theme.colors.white};
+        color: ${theme.colors.black};
+    `,
+
+    active: (theme: DefaultTheme) => css`
+        background: ${theme.colors.primary};
+        color: ${theme.colors.white};
+    `
+}
+
+export const Nav = styled.nav`
+    ${({ theme }) => css`
+        display: flex;
+        border-bottom: 0.1rem solid ${theme.colors.lightgray};
+
+        ${media.greaterThan('medium')`
+            flex-direction: column;
+            border: 0;
+
+            a:not(:last-child) {
+                border-bottom: 0.1rem solid ${theme.colors.lightgray}
+            }
+
+        `}
+    `}
+`
+
+export const Link = styled.a<LinkProps>`
+    ${({ theme, isActive }) => css`
+        padding: ${theme.spacings.xsmall} ${theme.spacings.small};
+        text-decoration: none;
+        transition: background, color, ${theme.transition.default};
+
+        display: flex;
+        align-items: center;
+
+        &:hover {
+            background: ${theme.colors.primary};
+            color: ${theme.colors.white};
+        }
+
+        > span {
+            margin-left: ${theme.spacings.xsmall};
+        }
+
+        ${media.lessThan('medium')`
+            justify-content: center;
+            flex: 1;
+
+            > span {
+                display: none;
+            }
+        `}
+
+        ${!isActive && linkModifiers.default(theme)}
+        ${isActive && linkModifiers.active(theme)}
+    `}
+`
