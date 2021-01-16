adf59f9d3044b8a3bfe5702b04ed0a5fe12b3926
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2021-01-16 19:28:51 +0000

    feat: Implement Profile temlate unit test

---
+++ src/templates/Profile/test.tsx
@@ -0,0 +1,45 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Profile from '.'
+
+jest.mock('next/router', () => ({
+    useRouter: jest.fn(() => ({ asPath: '/profile/me' }))
+}))
+
+jest.mock('templates/Base', () => {
+    return {
+        __esModule: true,
+        default: function Mock({ children }: { children: React.ReactNode }) {
+            return <div data-testid="Mock Base">⁄{children}</div>
+        }
+    }
+})
+
+jest.mock('components/Heading', () => {
+    return {
+        __esModule: true,
+        default: function Mock({ children }: { children: React.ReactNode }) {
+            return <div data-testid="Mock Heading">⁄{children}</div>
+        }
+    }
+})
+
+jest.mock('components/ProfileMenu', () => {
+    return {
+        __esModule: true,
+        default: function Mock() {
+            return <div data-testid="Mock ProfileMenu" />
+        }
+    }
+})
+
+describe('<Profile />', () => {
+    it('should render the sections', () => {
+        renderWithTheme(<Profile>Lorem Ipsum</Profile>)
+
+        expect(screen.getByText(/lorem ipsum/i)).toBeInTheDocument()
+        expect(screen.getByText(/my profile/i)).toBeInTheDocument()
+        expect(screen.getByTestId(/mock profilemenu/i)).toBeInTheDocument()
+    })
+})
