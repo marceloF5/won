fbe404db3935e9383487d637e60624eb21bcdb5e
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2021-01-23 14:07:07 +0000

    feat: Create structure ExplorerSidebar stories

---
+++ src/components/ExplorerSidebar/stories.tsx
@@ -0,0 +1,9 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import ExplorerSidebar from '.'
+
+export default {
+    title: 'ExplorerSidebar',
+    component: ExplorerSidebar
+} as Meta
+
+export const Default: Story = () => <ExplorerSidebar />
