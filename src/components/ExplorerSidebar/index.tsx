531980a0aa013e6f551e793092a11c469843b40a
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2021-01-24 10:25:42 +0000

    feat: Rename component

--- src/components/ExplorerSidebar/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as S from './styles'
-
-const ExplorerSidebar = () => (
-    <S.Wrapper>
-        <h1>ExplorerSidebar</h1>
-    </S.Wrapper>
-)
-
-export default ExplorerSidebar
--- src/components/ExplorerSidebar/stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Story, Meta } from '@storybook/react/types-6-0'
-import ExplorerSidebar from '.'
-
-export default {
-    title: 'ExplorerSidebar',
-    component: ExplorerSidebar
-} as Meta
-
-export const Default: Story = () => <ExplorerSidebar />
