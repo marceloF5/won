c4d8c41b33a7d49a09d0b48b2c31b23c9dc72d02
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2021-01-26 10:45:28 +0000

    feat: Add CartDropdown stories

---
+++ src/components/CartDropdown/stories.tsx
@@ -0,0 +1,26 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import CartDropdown, { CartDropdownProps } from '.'
+
+import items from 'components/CartList/mock'
+
+export default {
+    title: 'CartDropdown',
+    component: CartDropdown,
+    parameters: {
+        backgrounds: {
+            default: 'won-dark'
+        }
+    },
+    args: {
+        items,
+        total: '€ 300.00'
+    }
+} as Meta
+
+export const Default: Story<CartDropdownProps> = (args) => (
+    <div
+        style={{ maxWidth: '98%', display: 'flex', justifyContent: 'flex-end' }}
+    >
+        <CartDropdown {...args} />
+    </div>
+)
