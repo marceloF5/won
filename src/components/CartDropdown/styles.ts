fd26e12e1a5aaff39d5f7dbe255769ffe66d0d10
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2021-01-26 10:45:50 +0000

    feat: Add CartDropdown styles

---
+++ src/components/CartDropdown/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+import { Wrapper as CartWrapperStyles } from 'components/CartList/styles'
+
+export const Wrapper = styled.div`
+    ${CartWrapperStyles} {
+        width: 56rem;
+    }
+`
