691788e936eec7881b29f30b1417d644bfbdec3e
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2021-01-29 09:47:30 +0000

    feat: Add CartList component

--- src/components/CartList/index.tsx
+++ src/components/CartList/index.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+import Button from 'components/Button'
 import GameItem, { GameItemProps } from 'components/GameItem'
 import React from 'react'
 
@@ -6,16 +8,23 @@ import * as S from './styles'
 export type CartListProps = {
     items: GameItemProps[]
     total: string
+    hasButton?: boolean
 }
 
-const CardList = ({ items, total }: CartListProps) => (
+const CardList = ({ items, total, hasButton = false }: CartListProps) => (
     <S.Wrapper>
         {items.map((item) => (
             <GameItem key={item.title} {...item} />
         ))}
 
         <S.Footer>
-            Total <S.Total>{total}</S.Total>
+            {!hasButton && <span>Total:</span>}
+            <S.Total>{total}</S.Total>
+            {hasButton && (
+                <Link href="/cart">
+                    <Button as="a">Buy it now</Button>
+                </Link>
+            )}
         </S.Footer>
     </S.Wrapper>
 )
