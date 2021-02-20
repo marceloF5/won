0af26d5bf81a2bb4739dd3546e052ec29709947b
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2021-02-20 16:11:40 +0000

    feat: Add Empty component when cart is empty

--- src/components/CartList/index.tsx
+++ src/components/CartList/index.tsx
@@ -1,31 +1,41 @@
 import Link from 'next/link'
 import Button from 'components/Button'
 import GameItem, { GameItemProps } from 'components/GameItem'
-import React from 'react'
+import Empty from 'components/Empty'
 
 import * as S from './styles'
 
 export type CartListProps = {
-    items: GameItemProps[]
-    total: string
+    items?: GameItemProps[]
+    total?: string
     hasButton?: boolean
 }
 
-const CardList = ({ items, total, hasButton = false }: CartListProps) => (
-    <S.Wrapper>
-        {items.map((item) => (
-            <GameItem key={item.title} {...item} />
-        ))}
+const CardList = ({ items = [], total, hasButton = false }: CartListProps) => (
+    <S.Wrapper isEmpty={!items.length}>
+        {items.length ? (
+            <>
+                {items.map((item) => (
+                    <GameItem key={item.title} {...item} />
+                ))}
 
-        <S.Footer>
-            {!hasButton && <span>Total:</span>}
-            <S.Total>{total}</S.Total>
-            {hasButton && (
-                <Link href="/cart">
-                    <Button as="a">Buy it now</Button>
-                </Link>
-            )}
-        </S.Footer>
+                <S.Footer>
+                    {!hasButton && <span>Total:</span>}
+                    <S.Total>{total}</S.Total>
+                    {hasButton && (
+                        <Link href="/cart">
+                            <Button as="a">Buy it now</Button>
+                        </Link>
+                    )}
+                </S.Footer>
+            </>
+        ) : (
+            <Empty
+                title="Your cart is empty"
+                description="Go back to the store and explore great games and offers"
+                hasLink
+            />
+        )}
     </S.Wrapper>
 )
 
