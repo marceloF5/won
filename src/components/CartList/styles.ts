bcdc4a9b5407c58f190a2717f745ed19d16f94ff
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2021-02-20 16:13:06 +0000

    feat: Update styles

--- src/components/CartList/styles.ts
+++ src/components/CartList/styles.ts
@@ -2,12 +2,38 @@ import styled, { css } from 'styled-components'
 import { tint } from 'polished'
 import media from 'styled-media-query'
 
-export const Wrapper = styled.main`
-    ${({ theme }) => css`
+import * as EmptyStyles from 'components/Empty/styles'
+
+type WrapperProps = {
+    isEmpty: boolean
+}
+
+export const Wrapper = styled.main<WrapperProps>`
+    ${({ theme, isEmpty }) => css`
         background: ${theme.colors.white};
         display: flex;
         flex-direction: column;
         align-self: start;
+
+        ${isEmpty &&
+        css`
+            ${EmptyStyles.Wrapper} {
+                padding-bottom: ${theme.spacings.medium};
+            }
+
+            ${EmptyStyles.Image} {
+                max-width: 20rem;
+            }
+
+            ${EmptyStyles.Title} {
+                font-size: ${theme.font.sizes.large};
+            }
+
+            ${EmptyStyles.Description} {
+                color: ${theme.colors.black};
+                font-size: ${theme.font.sizes.medium};
+            }
+        `}
     `}
 `
 
