c41ce93fd41f3dc40ccce595e915fb646bd5fd7d
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2021-02-23 21:10:12 +0000

    feat: Add slug and Link

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import {
     Favorite,
     FavoriteBorder,
@@ -7,14 +8,16 @@ import {
 import Button from 'components/Button'
 import Ribbon, { RibbonColors, RibbonSizes } from 'components/Ribbon'
 import * as S from './styles'
+import formatPrice from 'utils/format-price'
 
 export type GameCardProps = {
+    slug: string
     title: string
     developer: string
     img: string
-    price: string
+    price: number
     favorite?: boolean
-    promotionalPrice?: string
+    promotionalPrice?: number
     ribbon?: React.ReactNode
     ribbonColor?: RibbonColors
     ribbonSize?: RibbonSizes
@@ -22,6 +25,7 @@ export type GameCardProps = {
 }
 
 const GameCard = ({
+    slug,
     title,
     developer,
     img,
@@ -39,14 +43,18 @@ const GameCard = ({
                 {ribbon}
             </Ribbon>
         )}
-        <S.ImageBox>
-            <img src={img} alt={title} />
-        </S.ImageBox>
+        <Link href={`game/${slug}`} passHref>
+            <S.ImageBox>
+                <img src={img} alt={title} />
+            </S.ImageBox>
+        </Link>
         <S.Content>
-            <S.Info>
-                <S.Title>{title}</S.Title>
-                <S.Developer>{developer}</S.Developer>
-            </S.Info>
+            <Link href={`game/${slug}`} passHref>
+                <S.Info>
+                    <S.Title>{title}</S.Title>
+                    <S.Developer>{developer}</S.Developer>
+                </S.Info>
+            </Link>
             <S.FavButton onClick={onFav} role="button">
                 {favorite ? (
                     <Favorite aria-label="Remove from Wishlist" />
@@ -55,8 +63,10 @@ const GameCard = ({
                 )}
             </S.FavButton>
             <S.BuyBox>
-                {!!promotionalPrice && <S.Price isPromotional>{price}</S.Price>}
-                <S.Price>{promotionalPrice || price}</S.Price>
+                {!!promotionalPrice && (
+                    <S.Price isPromotional>{formatPrice(price)}</S.Price>
+                )}
+                <S.Price>{formatPrice(promotionalPrice || price)}</S.Price>
                 <Button icon={<AddShoppingCart />} size="small" />
             </S.BuyBox>
         </S.Content>
