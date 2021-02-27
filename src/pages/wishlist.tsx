5916c251bb52ff1457828e3758ee19365c77fc29
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2021-02-27 13:45:17 +0000

    feat: Add query to recommended games

--- src/pages/wishlist.tsx
+++ src/pages/wishlist.tsx
@@ -1,18 +1,33 @@
 import Wishlist, { WishlistTemplateProps } from 'templates/Wishlist'
 
+import { initializeApollo } from 'utils/apollo'
+import { QueryRecommended } from 'graphql/generated/QueryRecommended'
+import { QUERY_RECOMMENDED } from 'graphql/queries/recommended'
+import { gamesMapper, highLightMapper } from 'utils/mappers'
+
 import gamesMock from 'components/GameCardSlider/mock'
-import highlightMock from 'components/Highlight/mock'
 
 export default function WishlistPage(props: WishlistTemplateProps) {
     return <Wishlist {...props} />
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
+    const apolloClient = initializeApollo()
+
+    const {
+        data: { recommended }
+    } = await apolloClient.query<QueryRecommended>({
+        query: QUERY_RECOMMENDED
+    })
+
     return {
         props: {
             games: gamesMock,
-            recommendedGames: gamesMock.slice(0, 5),
-            recommendedHighlight: highlightMock
+            recommendedGamesTitle: recommended?.section!.title,
+            recommendedGames: gamesMapper(recommended?.section?.games),
+            recommendedHighlight: highLightMapper(
+                recommended?.section?.highlight
+            )
         }
     }
 }
