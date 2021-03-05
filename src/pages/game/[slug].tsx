87865010cb0408c51065b90e51326b8708bcd03e
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2021-03-05 20:08:38 +0000

    feat: Add initialize with request in game page

--- src/pages/game/[slug].tsx
+++ src/pages/game/[slug].tsx
@@ -1,57 +1,104 @@
+import { useRouter } from 'next/router'
+import { GetStaticProps } from 'next'
+import { initializeApollo } from 'utils/apollo'
 import Game, { GameTemplateProps } from 'templates/Game'
-import galleryMock from 'components/Gallery/mock'
-import gameDetailsMock from 'components/GameDetails/mock'
-import gamesMock from 'components/GameCardSlider/mock'
-import highlightMock from 'components/Highlight/mock'
+import { QueryGames, QueryGamesVariables } from 'graphql/generated/QueryGames'
+import { QUERY_GAMES, QUERY_GAME_BY_SLUG } from 'graphql/queries/games'
+import {
+    QueryGameBySlug,
+    QueryGameBySlugVariables
+} from 'graphql/generated/QueryGameBySlug'
+import { QueryRecommended } from 'graphql/generated/QueryRecommended'
+import { QUERY_RECOMMENDED } from 'graphql/queries/recommended'
+import { gamesMapper, highLightMapper } from 'utils/mappers'
+import {
+    QueryUpcoming,
+    QueryUpcomingVariables
+} from 'graphql/generated/QueryUpcoming'
+import { QUERY_UPCOMING } from 'graphql/queries/upcoming'
+
+const apolloClient = initializeApollo()
 
 export default function Index(props: GameTemplateProps) {
+    const router = useRouter()
+
+    // if the page is not loaded the fallback is true and you could return some skeleton here
+    if (router.isFallback) return null
+
     return <Game {...props} />
 }
 
 export async function getStaticPaths() {
-    return {
-        paths: [{ params: { slug: 'cyberpunk-2077' } }],
-        fallback: false
-    }
+    const { data } = await apolloClient.query<QueryGames, QueryGamesVariables>({
+        query: QUERY_GAMES,
+        variables: { limit: 9 }
+    })
+
+    const paths = data.games.map(({ slug }) => ({ params: { slug } }))
+
+    return { paths, fallback: true }
 }
 
 // Generate this page in build time
-export async function getStaticProps() {
-    const descriptionHTML = `
-        <img src="https://items.gog.com/not_a_cp/ENG_product-page-addons-2020_yellow_on_black.png"><br>
-        * Exclusive Digital Comic - Cyberpunk 2077: Big City Dreams will be available in English only.
-        <hr><p class="module">Korean Voiceover will be added on 11th December 2020.</p><br><img alt="" src="https://items.gog.com/not_a_cp/EN/EN-About-the-Game.png"><br><br><b>Cyberpunk 2077</b> is an open-world, action-adventure story set in Night City, a megalopolis obsessed with power, glamour and body modification. You play as V, a mercenary outlaw going after a one-of-a-kind implant that is the key to immortality. You can customize your character’s cyberware, skillset and playstyle, and explore a vast city where the choices you make shape the story and the world around you.
-        <br><br><img alt="" src="https://items.gog.com/not_a_cp/EN/EN-Mercenary-Outlaw.png"><br><br>
-        Become a cyberpunk, an urban mercenary equipped with cybernetic enhancements and build your legend on the streets of Night City.
-        <br><br><img alt="" src="https://items.gog.com/not_a_cp/EN/EN-City-of-the-Future.png"><br><br>
-        Enter the massive open world of Night City, a place that sets new standards in terms of visuals, complexity and depth.
-        <br><br><img alt="" src="https://items.gog.com/not_a_cp/EN/EN-Eternal-Life.png"><br><br>
-        Take the riskiest job of your life and go after a prototype implant that is the key to immortality.
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+    const {
+        data: { games }
+    } = await apolloClient.query<QueryGameBySlug, QueryGameBySlugVariables>({
+        query: QUERY_GAME_BY_SLUG,
+        variables: { slug: `${params?.slug}` },
+        fetchPolicy: 'no-cache'
+    })
 
+    if (!games.length) {
+        return { notFound: true }
+    }
 
-        <p class="description__copyrights">
-        CD PROJEKT®, Cyberpunk®, Cyberpunk 2077® are registered trademarks of CD PROJEKT S.A. © 2019
-        CD PROJEKT S.A. All rights reserved. All other copyrights and trademarks are the property of their
-        respective owners.
-    </p>`
+    const game = games[0]
+
+    const TODAY = new Date().toISOString().slice(0, 10) // yyyy-MM-dd
+    const {
+        data: { upcomingGames, sections }
+    } = await apolloClient.query<QueryUpcoming, QueryUpcomingVariables>({
+        query: QUERY_UPCOMING,
+        variables: { date: TODAY },
+        fetchPolicy: 'no-cache'
+    })
+
+    const {
+        data: { recommended }
+    } = await apolloClient.query<QueryRecommended>({
+        query: QUERY_RECOMMENDED
+    })
 
     return {
+        revalidate: 60,
         props: {
-            cover:
-                'https://images.gog-statics.com/5643a7c831df452d29005caeca24c28cdbfaa6fbea5a9556b147ee26d325fa70_bg_crop_1366x655.jpg',
+            cover: `http://localhost:1337${game.cover?.src}`,
             gameInfo: {
-                title: 'Cyberpunk 2077',
-                price: '59.00',
-                description: `Cyberpunk 2077 is an open-world, action-adventure story set in Night City, a megalopolis obsessed with
-                    power, glamour and body modification. You play as V, a mercenary outlaw goint after a one-of-a-kind implant
-                    that is  the key to immortality`
+                title: game.name,
+                price: game.price,
+                description: game.short_description
             },
-            gallery: galleryMock,
-            description: descriptionHTML,
-            details: gameDetailsMock,
-            upcomingGames: gamesMock,
-            upcomingHighlight: highlightMock,
-            recommendedGames: gamesMock
+            gallery: game.gallery.map((image) => ({
+                src: `http://localhost:1337${image.src}`,
+                label: image.label
+            })),
+            description: game.description,
+            details: {
+                developers: game.developers[0].name,
+                releaseDate: game.release_date,
+                platforms: game.platforms.map((platform) => platform.name),
+                publicher: game.publisher?.name,
+                rating: game.rating,
+                genres: game.categories.map((category) => category.name)
+            },
+            upcomingTitle: sections?.upcomingGames?.title,
+            upcomingHighlight: highLightMapper(
+                sections?.upcomingGames?.highlight
+            ),
+            upcomingGames: gamesMapper(upcomingGames),
+            recommendedGamesTitle: recommended?.section!.title,
+            recommendedGames: gamesMapper(recommended?.section?.games)
         }
     }
 }
