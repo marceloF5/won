2ef5be9dafd934d2e8389a74c34ed852dcdefa2d
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2021-03-05 20:10:24 +0000

    feat: Add apolloProvider in App

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,27 +1,31 @@
-import { AppProps } from 'next/app'
 import Head from 'next/head'
-import React from 'react'
+import { AppProps } from 'next/app'
+import { ApolloProvider } from '@apollo/client'
 import { ThemeProvider } from 'styled-components'
 
 import GlobalStyles from 'styles/global'
 import theme from 'styles/theme'
+import { useApollo } from 'utils/apollo'
 
 function App({ Component, pageProps }: AppProps) {
+    const client = useApollo(pageProps.initialApolloState)
     return (
-        <ThemeProvider theme={theme}>
-            <Head>
-                <title>Won Games</title>
-                <link rel="shortcut icon" href="/img/icon-512.png" />
-                <link rel="apple-touch-icon" href="/img/icon-512.png" />
-                <link rel="manifest" href="/manifest.json" />
-                <meta
-                    name="description"
-                    content="The best Game Stores in the world"
-                />
-            </Head>
-            <GlobalStyles />
-            <Component {...pageProps} />
-        </ThemeProvider>
+        <ApolloProvider client={client}>
+            <ThemeProvider theme={theme}>
+                <Head>
+                    <title>Won Games</title>
+                    <link rel="shortcut icon" href="/img/icon-512.png" />
+                    <link rel="apple-touch-icon" href="/img/icon-512.png" />
+                    <link rel="manifest" href="/manifest.json" />
+                    <meta
+                        name="description"
+                        content="The best Game Stores in the world"
+                    />
+                </Head>
+                <GlobalStyles />
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </ApolloProvider>
     )
 }
 
