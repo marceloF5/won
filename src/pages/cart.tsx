a6f12ec7f366956d193e5804ff37700155e30eba
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2021-03-05 20:12:16 +0000

    feat: Add initialize with request in cart page

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,22 +1,35 @@
 import Cart, { CartProps } from 'templates/Cart'
 
 import cardMock from 'components/PaymentOptions/mock'
-import gameMock from 'components/GameCardSlider/mock'
 import itemsMock from 'components/CartList/mock'
-import highlightMock from 'components/Highlight/mock'
+import { initializeApollo } from 'utils/apollo'
+import { QueryRecommended } from 'graphql/generated/QueryRecommended'
+import { QUERY_RECOMMENDED } from 'graphql/queries/recommended'
+import { gamesMapper, highLightMapper } from 'utils/mappers'
 
 export default function CartPage(props: CartProps) {
     return <Cart {...props} />
 }
 
 export async function getServerSideProps() {
+    const apolloClient = initializeApollo()
+
+    const {
+        data: { recommended }
+    } = await apolloClient.query<QueryRecommended>({
+        query: QUERY_RECOMMENDED
+    })
+
     return {
         props: {
             items: itemsMock,
             total: '€480.00',
             cards: cardMock,
-            recommendedGames: gameMock,
-            recommendedHighlight: highlightMock
+            recommendedGamesTitle: recommended?.section!.title,
+            recommendedGames: gamesMapper(recommended?.section?.games),
+            recommendedHighlight: highLightMapper(
+                recommended?.section?.highlight
+            )
         }
     }
 }
