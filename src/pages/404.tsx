b27d1ca6d3e5bebe6bfa1e193251d1597ddb2f56
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2021-03-05 20:11:31 +0000

    feat: Add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,17 @@
+import Base from 'templates/Base'
+import { Container } from 'components/Container'
+import Empty from 'components/Empty'
+
+export default function Page404() {
+    return (
+        <Base>
+            <Container>
+                <Empty
+                    title="404: Not found"
+                    description="Ops... this page does not exist. Go back to the store and enjoy our offers"
+                    hasLink
+                />
+            </Container>
+        </Base>
+    )
+}
