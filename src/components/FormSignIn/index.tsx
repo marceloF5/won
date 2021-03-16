6deb0f8b874b5ba11ca0e90bbc7ea2f20d67ff1f
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2021-03-16 10:30:34 +0000

    feat: Implements form sign in

--- src/components/FormSignIn/index.tsx
+++ src/components/FormSignIn/index.tsx
@@ -1,39 +1,95 @@
-import { Email, Lock } from '@styled-icons/material-outlined'
+import { signIn } from 'next-auth/client'
+import { useRouter } from 'next/router'
+import { Email, Lock, ErrorOutline } from '@styled-icons/material-outlined'
+import { FieldErrors, signInValidate } from 'utils/validations'
+import { FormWrapper, FormLink, FormLoading, FormError } from 'components/Form'
+import React, { useState } from 'react'
+import Button from 'components/Button'
 import Link from 'next/link'
+import TextField from 'components/TextField'
 
 import * as S from './styles'
-import Button from 'components/Button'
-import { FormWrapper, FormLink } from 'components/Form'
-import TextField from 'components/TextField'
-import React from 'react'
 
-const FormSignIn = () => (
-    <FormWrapper>
-        <form action="">
-            <TextField
-                name="email"
-                placeholder="Email"
-                type="email"
-                icon={<Email />}
-            />
-            <TextField
-                name="password"
-                placeholder="Password"
-                type="password"
-                icon={<Lock />}
-            />
-            <S.ForgotPassword href="#">Forgot your password?</S.ForgotPassword>
-            <Button size="large" fullWidth>
-                Sign in now
-            </Button>
-            <FormLink>
-                Do not have an account?{' '}
-                <Link href="/sign-up">
-                    <a>Sign up</a>
-                </Link>
-            </FormLink>
-        </form>
-    </FormWrapper>
-)
+const FormSignIn = () => {
+    const [values, setValues] = useState({ email: '', password: '' })
+    const [loading, setLoading] = useState(false)
+    const [fieldError, setFieldErrors] = useState<FieldErrors>({})
+    const [formError, setFormError] = useState('')
+    const routes = useRouter()
+    const { push, query } = routes
+
+    const handleInput = (field: string, value: string) => {
+        setValues((s) => ({ ...s, [field]: value }))
+    }
+
+    const handleSubmit = async (event: React.FormEvent) => {
+        event.preventDefault()
+        setLoading(true)
+
+        const errors = signInValidate(values)
+
+        if (Object.keys(errors).length) {
+            setFieldErrors(errors)
+            setLoading(false)
+            return
+        }
+
+        setFieldErrors({})
+
+        const result = await signIn('credentials', {
+            ...values,
+            redirect: false,
+            callbackUrl: `${window.location.origin}${query?.callbackUrl || ''}`
+        })
+
+        if (result?.url) {
+            return push(result?.url)
+        }
+
+        setLoading(false)
+        setFormError('username or password is invalid')
+    }
+
+    return (
+        <FormWrapper>
+            {!!formError && (
+                <FormError>
+                    <ErrorOutline size={10} />
+                    {formError}
+                </FormError>
+            )}
+            <form onSubmit={handleSubmit}>
+                <TextField
+                    name="email"
+                    placeholder="Email"
+                    type="email"
+                    error={fieldError?.email}
+                    icon={<Email />}
+                    onInputChange={(v) => handleInput('email', v)}
+                />
+                <TextField
+                    name="password"
+                    placeholder="Password"
+                    type="password"
+                    error={fieldError?.password}
+                    icon={<Lock />}
+                    onInputChange={(v) => handleInput('password', v)}
+                />
+                <S.ForgotPassword href="#">
+                    Forgot your password?
+                </S.ForgotPassword>
+                <Button type="submit" size="large" fullWidth disabled={loading}>
+                    {loading ? <FormLoading /> : <span>Sign in now</span>}
+                </Button>
+                <FormLink>
+                    Do not have an account?{' '}
+                    <Link href="/sign-up">
+                        <a>Sign up</a>
+                    </Link>
+                </FormLink>
+            </form>
+        </FormWrapper>
+    )
+}
 
 export default FormSignIn
