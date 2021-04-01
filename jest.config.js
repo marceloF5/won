99ae26ee54c54f852cc4cdbc1c9de5cb2898e803
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2021-04-01 12:17:43 +0100

    feat: Update jest.config

--- jest.config.js
+++ jest.config.js
@@ -6,9 +6,11 @@ module.exports = {
         'src/**/*.ts(x)?',
         '!src/**/stories.tsx',
         '!src/pages/**/*.tsx',
+        '!src/pages/**/*.ts',
         '!src/styles/**/*.ts',
         '!src/utils/apollo.ts',
         '!src/utils/apolloCache.ts',
+        '!src/utils/protected-routes.ts',
         '!src/types/**/*.d.ts',
         '!src/graphql/**/*.ts',
         '!src/**/mock.ts'
