4953d0d6d0ccb1813a28f03f7f6abb3ebfef921a
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2021-05-16 10:12:01 +0100

    chore: Request user info

--- src/pages/profile/me.tsx
+++ src/pages/profile/me.tsx
@@ -1,20 +1,32 @@
 import { GetServerSidePropsContext } from 'next'
-import FormProfile from 'components/FormProfile'
+import FormProfile, { FormProfileProps } from 'components/FormProfile'
 import Profile from 'templates/Profile'
 import protectedRoutes from 'utils/protected-routes'
+import { initializeApollo } from 'utils/apollo'
+import { QueryProfileMe } from 'graphql/generated/QueryProfileMe'
+import { QUERY_PROFILE_ME } from 'graphql/queries/profile'
 
-export default function Me() {
+export default function Me(props: FormProfileProps) {
     return (
         <Profile>
-            <FormProfile />
+            <FormProfile {...props} />
         </Profile>
     )
 }
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
     const session = await protectedRoutes(context)
+    const apolloClient = initializeApollo(null, session)
+
+    const { data } = await apolloClient.query<QueryProfileMe>({
+        query: QUERY_PROFILE_ME
+    })
 
     return {
-        props: { session }
+        props: {
+            session,
+            username: data.me?.username,
+            email: data.me?.email
+        }
     }
 }
