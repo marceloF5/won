d351b7c11e8a0a86ff756b57844d8428cebcf764
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2021-05-16 10:12:51 +0100

    chore: Improve Base Layout

--- src/templates/Base/index.tsx
+++ src/templates/Base/index.tsx
@@ -4,25 +4,30 @@ import * as S from './styles'
 import { Container } from 'components/Container'
 import Footer from 'components/Footer'
 import Menu from 'components/Menu'
+import { useSession } from 'next-auth/client'
 
 export type BaseTemplateProps = {
     children: React.ReactNode
 }
 
-const Base = ({ children }: BaseTemplateProps) => (
-    <S.Wrapper>
-        <Container>
-            <Menu username="marcelo" />
-        </Container>
+const Base = ({ children }: BaseTemplateProps) => {
+    const [session, loading] = useSession()
 
-        <S.Content>{children}</S.Content>
-
-        <S.SectionFooter>
+    return (
+        <S.Wrapper>
             <Container>
-                <Footer />
+                <Menu username={session?.user?.name} loading={loading} />
             </Container>
-        </S.SectionFooter>
-    </S.Wrapper>
-)
+
+            <S.Content>{children}</S.Content>
+
+            <S.SectionFooter>
+                <Container>
+                    <Footer />
+                </Container>
+            </S.SectionFooter>
+        </S.Wrapper>
+    )
+}
 
 export default Base
