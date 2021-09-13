da704626f850fcd8fa2b15e8bac0988ebe9327f7
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2021-09-13 09:28:20 +0100

    chore: Consume CreditCard component

--- src/components/ProfileMenu/index.tsx
+++ src/components/ProfileMenu/index.tsx
@@ -1,6 +1,5 @@
 import {
     AccountCircle,
-    CreditCard,
     FormatListBulleted,
     ExitToApp
 } from '@styled-icons/material-outlined'
@@ -19,13 +18,6 @@ const ProfileMenu = ({ activeLink }: ProfileMenuProps) => (
             <S.Link isActive={activeLink === '/profile/me'} title="My profile">
                 <AccountCircle size={24} />
                 <span>My profile</span>
-            </S.Link>
-        </Link>
-
-        <Link href="/profile/cards" passHref>
-            <S.Link isActive={activeLink === '/profile/cards'} title="My cards">
-                <CreditCard size={24} />
-                <span>My cards</span>
             </S.Link>
         </Link>
 
--- src/components/ProfileMenu/test.tsx
+++ src/components/ProfileMenu/test.tsx
@@ -9,12 +9,11 @@ describe('<ProfileMenu />', () => {
         expect(
             screen.getByRole('link', { name: /my profile/i })
         ).toBeInTheDocument()
-        expect(
-            screen.getByRole('link', { name: /my cards/i })
-        ).toBeInTheDocument()
+
         expect(
             screen.getByRole('link', { name: /my orders/i })
         ).toBeInTheDocument()
+
         expect(
             screen.getByRole('button', { name: /sign out/i })
         ).toBeInTheDocument()
@@ -23,7 +22,7 @@ describe('<ProfileMenu />', () => {
     })
 
     it('should render the menu with an active link defined', () => {
-        render(<ProfileMenu activeLink="/profile/cards" />)
+        render(<ProfileMenu activeLink="/profile/orders" />)
 
         expect(screen.getByRole('link', { name: /my cards/i })).toHaveStyle({
             background: '#f231a5',
