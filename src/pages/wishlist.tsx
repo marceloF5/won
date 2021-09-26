9b19c3301ad0345ddeff085b787c893e4b86204d
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2021-09-26 21:15:29 +0100

    feat: Update wishlist page

--- src/pages/wishlist.tsx
+++ src/pages/wishlist.tsx
@@ -1,11 +1,16 @@
 import { initializeApollo } from 'utils/apollo'
-import { gamesMapper, highLightMapper } from 'utils/mappers'
+import { gamesMapper, highlightMapper } from 'utils/mappers'
 import { GetServerSidePropsContext } from 'next'
 import { QueryRecommended } from 'graphql/generated/QueryRecommended'
 import { QUERY_RECOMMENDED } from 'graphql/queries/recommended'
 import gamesMock from 'components/GameCardSlider/mock'
 import protectedRoutes from 'utils/protected-routes'
 import Wishlist, { WishlistTemplateProps } from 'templates/Wishlist'
+import { QUERY_WISHLIST } from 'graphql/queries/wishlist'
+import {
+    QueryWishlist,
+    QueryWishlistVariables
+} from 'graphql/generated/QueryWishlist'
 
 export default function WishlistPage(props: WishlistTemplateProps) {
     return <Wishlist {...props} />
@@ -13,7 +18,16 @@ export default function WishlistPage(props: WishlistTemplateProps) {
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
     const session = await protectedRoutes(context)
-    const apolloClient = initializeApollo()
+    const apolloClient = initializeApollo(null, session)
+
+    if (!session) return {}
+
+    await apolloClient.query<QueryWishlist, QueryWishlistVariables>({
+        query: QUERY_WISHLIST,
+        variables: {
+            identifier: session.user.email as string
+        }
+    })
 
     const {
         data: { recommended }
@@ -24,10 +38,11 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
     return {
         props: {
             session,
+            initialApolloState: apolloClient.cache.extract(),
             games: gamesMock,
             recommendedGamesTitle: recommended?.section!.title,
             recommendedGames: gamesMapper(recommended?.section?.games),
-            recommendedHighlight: highLightMapper(
+            recommendedHighlight: highlightMapper(
                 recommended?.section?.highlight
             )
         }
